1c303190b530cf178672443ae7f5fd78698fdc13
lambda231 <khanh.lams@hcmut.edu.vn>
2025-04-06 18:54:38 +0700

    Done authorization api.

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,7 +1,15 @@
 export function bitmapToBooleans(bitmap: number, length = 32): boolean[] {
-    const result: boolean[] = [];
-    for (let i = 0; i < length; i++) {
-      result.push((bitmap & (1 << i)) !== 0);
-    }
-    return result;
+  const result: boolean[] = [];
+  for (let i = 0; i < length; i++) {
+    result.push((bitmap & (1 << i)) !== 0);
   }
+  return result;
+}
+
+export const getApiUrl = (path: string) => {
+  const baseUrl =
+    typeof window === "undefined"
+      ? process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"
+      : "";
+  return `${baseUrl}${path}`;
+};
