d6931807a8c2344d5b8dd54b5a062349b8b7afe0
duyenln <duyenln@vietnamblockchain.asia>
2025-04-22 09:40:31 +0700

    remove comment

--- src/App.jsx
+++ src/App.jsx
@@ -23,9 +23,9 @@ function App() {
       <Route
         path="/room-booking"
         element={
-          // <ProtectedRoute>
+          <ProtectedRoute>
             <RoomBooking />
-          // </ProtectedRoute>
+          </ProtectedRoute>
         }
       />
     </Routes>
